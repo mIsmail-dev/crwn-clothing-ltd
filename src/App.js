015226bcbfdafd8c966861ceb86a454a4e575f31
ismailbutt89@gmail.com <muhammad.ismail@ocloudsolutions.net>
2022-10-28 15:22:03 +0500

    Added ProductContext & Shop Page.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/Home/Home'
 import Authentication from './pages/Authentication/Authentication'
+import Shop from './pages/shop/Shop'
 import { UserProvider } from './contexts/UserContext'
+import { ProductProvider } from './contexts/ProductContext'
 
 const App = () => {
   return (
     <UserProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/auth' element={<Authentication />} />
-        </Routes>
-      </Router>
+      <ProductProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/auth' element={<Authentication />} />
+            <Route path='/shop' element={<Shop />} />
+          </Routes>
+        </Router>
+      </ProductProvider>
     </UserProvider>
   )
 }
