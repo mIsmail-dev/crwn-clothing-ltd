52b1abc7e4f7e8094671fe1891a724c1c677e760
ismailbutt89@gmail.com <muhammad.ismail@ocloudsolutions.net>
2022-11-01 19:03:22 +0500

    Styled Component - Form Input Component.

--- src/components/form-input/FormInput.js
+++ src/components/form-input/FormInput.js
@@ -1,19 +1,17 @@
-import './formInput.scss'
+import { FormInputLabel, Input, Group } from './formInput.styles.js'
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className='group'>
-      <input className='form-input' {...otherProps} />
+    <Group>
+      <Input {...otherProps} />
       {label && (
-        <label
-          className={`${
-            otherProps.value && otherProps.value.length ? 'shrink' : ''
-          } form-input-label`}
+        <FormInputLabel
+          shrink={`${otherProps.value && otherProps.value.length}`}
         >
           {label}
-        </label>
+        </FormInputLabel>
       )}
-    </div>
+    </Group>
   )
 }
 
