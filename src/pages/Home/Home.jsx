45e125d4a85b9d5da853844fa008a0d54d77ec80
ismailbutt89@gmail.com <muhammad.ismail@ocloudsolutions.net>
2022-11-10 14:05:19 +0500

    Added title and meta tags in all pages.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
+import { Helmet } from 'react-helmet'
 import Directory from '../../components/directory/Directory'
 
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Home page</title>
+        <meta name='description' content='Showing all categories of store' />
+      </Helmet>
       <Directory />
     </div>
   )
