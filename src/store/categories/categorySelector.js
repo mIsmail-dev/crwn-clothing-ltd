288cb72841930d6ddb08015a37b59471c3ac58db
ismailbutt89@gmail.com <muhammad.ismail@ocloudsolutions.net>
2022-11-12 19:09:44 +0500

    Removed comments, Memorization using re-selector library is done.

--- src/store/categories/categorySelector.js
+++ src/store/categories/categorySelector.js
@@ -10,12 +10,10 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) => {
-    console.log('Selector Fired.')
-    return categories.reduce((acc, category) => {
+  (categories) =>
+    categories.reduce((acc, category) => {
       const { title, items } = category
       acc[title.toLowerCase()] = items
       return acc
     }, {})
-  }
 )
