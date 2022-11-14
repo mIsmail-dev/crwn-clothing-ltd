e76247076566ff1aa6292b2dceab9060e487e692
ismailbutt89@gmail.com <muhammad.ismail@ocloudsolutions.net>
2022-11-14 14:35:10 +0500

    Added required Config for Redux-Devtools.

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,9 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-// import logger from 'redux-logger'
+import logger from 'redux-logger'
 
 import { rootReducer } from './root-reducer'
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action)
-  }
-
-  console.log('type: ', action.type)
-  console.log('payload: ', action.payload)
-  console.log('current State: ', store.getState())
-
-  next(action)
-
-  console.log('next State: ', store.getState())
-}
 
 const persistConfig = {
   key: 'root',
@@ -27,11 +13,13 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [
-  process.env.NODE_ENV === 'development' && loggerMiddleware,
-].filter(Boolean)
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+)
 
-const composedEnhancers = compose(applyMiddleware(...middleWares))
+const composeEnhancher = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+const composedEnhancers = composeEnhancher(applyMiddleware(...middleWares))
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers)
 
