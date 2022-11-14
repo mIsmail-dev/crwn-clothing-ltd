ab6a4433eecd20a1e07e16d2436440278df51825
ismailbutt89@gmail.com <muhammad.ismail@ocloudsolutions.net>
2022-11-14 14:08:09 +0500

    added redux-persist.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
-import { store } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   // <React.StrictMode> // The error solved: Link: https://github.com/nfl/react-helmet/issues/548 bcz of react-helmet
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
   // </React.StrictMode>
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 // import logger from 'redux-logger'
 
 import { rootReducer } from './root-reducer'
@@ -17,10 +19,20 @@ const loggerMiddleware = (store) => (next) => (action) => {
   console.log('next State: ', store.getState())
 }
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const middleWares = [
   process.env.NODE_ENV === 'development' && loggerMiddleware,
 ].filter(Boolean)
 
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistor = persistStore(store)
